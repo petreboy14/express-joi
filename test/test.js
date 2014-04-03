dd570a3aca88537d03d3084be1f3c8fd38991f82
Peter Henning <petreboy14@gmail.com>
2014-04-03 10:54:42 -0700

    Fixing lint issues

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+/*global after,before,describe,it*/
 var express = require('express');
 var Joi = require('joi');
 var should = require('should');
@@ -5,59 +6,60 @@ var request = require('request');
 
 var expressJoi = require('../index');
 
-describe('express-joi tests', function() {
-  describe('expressJoi object', function() {
-    it('should not be null', function() {
+describe('express-joi tests', function () {
+  describe('expressJoi object', function () {
+    it('should not be null', function () {
       should.exist(expressJoi);
     });
-    it('should contain a joiValidate function', function() {
+    it('should contain a joiValidate function', function () {
       should.exist(expressJoi.joiValidate);
       expressJoi.joiValidate.should.be.an.instanceof(Function);
     });
-    it('should have a Joi object', function() {
+    it('should have a Joi object', function () {
       should.exist(expressJoi.Joi);
       expressJoi.Joi.should.equal(Joi);
     });
   });
 
-  describe('expressJoi.Joi', function() {
-    it('should be able to create a schema object with Joi', function() {
+  describe('expressJoi.Joi', function () {
+    it('should be able to create a schema object with Joi', function () {
       var schemaError = null;
+      var schema = null;
       try {
-        var schema = {
+        schema = {
           username: expressJoi.Joi.types.String().alphanum().min(3).max(30).with('birthyear').required(),
           password: expressJoi.Joi.types.String().regex(/[a-zA-Z0-9]{3,30}/).without('access_token'),
           access_token: expressJoi.Joi.types.String(),
           birthyear: expressJoi.Joi.types.Number().min(1850).max(2012),
           email: expressJoi.Joi.types.String().email()
         };
-      } catch(err) {
+      } catch (err) {
         schemaError = err;
       }
       should.exist(schema);
       should.not.exist(schemaError);
     });
   });
-  
-  describe('expressJoi.joiValidate', function() {
+
+  describe('expressJoi.joiValidate', function () {
     var app = null;
     var server = null;
 
-    before(function(done) {
+    before(function (done) {
       app = express();
       app.use(express.methodOverride());
       app.use(express.bodyParser());
       app.use(app.router);
-      app.use(function(err, req, res, next) {
+      app.use(function (err, req, res, next) {
         res.send(500, {message: err.message});
       });
 
-      server = app.listen(8181, function() {
+      server = app.listen(8181, function () {
         done();
       });
     });
 
-    it('should be able to attach a validator to a specific route', function() {
+    it('should be able to attach a validator to a specific route', function () {
       var error = null;
 
       try {
@@ -67,17 +69,17 @@ describe('express-joi tests', function() {
           name: expressJoi.Joi.types.String().alphanum().min(2).max(25)
         };
 
-        app.get('/users', expressJoi.joiValidate(schema), function returnFunc(req, res) { 
+        app.get('/users', expressJoi.joiValidate(schema), function returnFunc(req, res) {
           res.send(200, { hello: 'world' });
         });
-      } catch(err) {
+      } catch (err) {
         error = err;
       }
 
       should.not.exist(error);
     });
-    it('should pass successfully through route with correct validation', function(done) {
-      request.get('http://localhost:8181/users?limit=5&offset=5&name=tom', function(err, res, body) {
+    it('should pass successfully through route with correct validation', function (done) {
+      request.get('http://localhost:8181/users?limit=5&offset=5&name=tom', function (err, res, body) {
         if (err) {
           done(err);
         }
@@ -86,7 +88,7 @@ describe('express-joi tests', function() {
 
         try {
           body = JSON.parse(body);
-        } catch(err) {
+        } catch (err) {
           done(err);
         }
 
@@ -95,8 +97,8 @@ describe('express-joi tests', function() {
         done();
       });
     });
-    it('should fail if an item does not have correct validation', function(done) {
-      request.get('http://localhost:8181/users?limit=-1&offset=5&name=tom', function(err, res, body) {
+    it('should fail if an item does not have correct validation', function (done) {
+      request.get('http://localhost:8181/users?limit=-1&offset=5&name=tom', function (err, res, body) {
         if (err) {
           done(err);
         }
@@ -105,7 +107,7 @@ describe('express-joi tests', function() {
 
         try {
           body = JSON.parse(body);
-        } catch(err) {
+        } catch (err) {
           done(err);
         }
 
@@ -115,7 +117,7 @@ describe('express-joi tests', function() {
       });
     });
 
-    it('should be able to pass strict: false option and still pass validation with unspecified parameter', function(done) {
+    it('should be able to pass strict: false option and still pass validation with unspecified parameter', function (done) {
       var schema = {
         limit: expressJoi.Joi.types.Number().integer().min(1).max(25),
         offset: expressJoi.Joi.types.Number().integer().min(0).max(25),
@@ -128,7 +130,7 @@ describe('express-joi tests', function() {
         items.foo.should.equal('bar');
         res.send(200, { hello: 'world' });
       });
-      request.get('http://localhost:8181/foos?foo=bar&offset=5&name=tom', function(err, res, body) {
+      request.get('http://localhost:8181/foos?foo=bar&offset=5&name=tom', function (err, res, body) {
         if (err) {
           done(err);
         }
@@ -137,7 +139,7 @@ describe('express-joi tests', function() {
       });
     });
 
-    after(function() {
+    after(function () {
       server.close();
     });
   });
